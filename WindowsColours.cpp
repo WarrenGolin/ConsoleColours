2190287fb2ee1df256b2c6d36e14202d6f9f988c
WarrenGolin <warren_here@hotmail.com>
2015-03-28 16:19:11 -0500

    Basic Colour options

---
+++ WindowsColours.cpp
@@ -0,0 +1,17 @@
+/* Windows colours
+ - Allows changes to the colours of characters printed to the win32 console
+ - Only characters written after method calls are altered
+
+ Programmed by Warren Golin
+ */
+#include "WindowsColours.h"
+
+void TextModifier::resetColour()
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), gray);
+}
+
+void TextModifier::setColour(colour c)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
